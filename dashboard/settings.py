50e68cabe8137d94380648284b5dcebc0bbed147
Roquelio <androroques03@gmail.com>
2024-02-22 21:28:16 -0500

    test

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-i028ej#%j(t9ojukl&mn7qxjroa1ef9%ro)b^oncw507t@(v*7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['dash2-3mqy.onrender.com', 'localhost']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dash2-3mqy.onrender.com', 'localhost']
 
 
 
